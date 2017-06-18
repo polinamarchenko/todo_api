a56725a1cfc2cd9aa14871ac0c5584fdf681041c
Polina Marchenko <polina.marchenko@yahoo.com>
2017-06-18 16:17:22 -0700

    Add test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,40 @@ describe('GET /todos/id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if id not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if ID is not valid', (done) => {
+    request(app)
+      .delete('/todos/1234abc')
+      .expect(404)
+      .end(done);
+  });
+});
