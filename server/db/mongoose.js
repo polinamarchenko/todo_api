d6934f7aa39f530a765d03d622abbf496b128319
Polina Marchenko <polina.marchenko@yahoo.com>
2017-06-18 13:15:35 -0700

    Adjust port setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.end.MONGODB_URI || 'mongodb://localhost:27017/UdemyTodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UdemyTodoApp');
 
 module.exports = {mongoose};
